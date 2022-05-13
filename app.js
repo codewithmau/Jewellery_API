b12fd8fbe9c528d9705273ed922e02dcc47fc236
codewithmau <97216746+codewithmau@users.noreply.github.com>
2022-05-13 23:44:00 +0530

    codeapi

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
 const mongoose = require("mongoose")
 const express = require("express")
 
-const shop = require("./Route/shopRoute")
-const user = require("./Route/userRoute")
-const login = require("./Route/loginRoute")
-
-const app = express()
+const msc = require("./Router/mscRouter")
 require("dotenv/config")
+const app = express()
+
 
 app.use(express.json())
 app.get("/",(req,res)=>{
-    res.send("Home Page")
+    res.send("Good Morning")
 })
 
-app.use("/shop",shop)
-app.use("/user",user)
-app.use("/login",login)
-
-app.listen(process.env.PORT || 4000);
+app.use("/msc",msc)
+app.listen(process.env.PORT || 5000);
 
 mongoose.connect(process.env.DB,{useNewUrlParser:true,useUnifiedTopology:true},()=>{
-    console.log("Database Connected Succefully..!");
+    console.log("Database Connected Successfully..!");
 })
